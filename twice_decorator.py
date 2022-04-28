2d2bece20f772086c4f44bd14fd168095e376385
Eliachar Feig <efeig15@gmail.com>
2022-04-28 14:31:20 +0300

    Documentation

--- twice_decorator.py
+++ twice_decorator.py
@@ -6,7 +6,19 @@ class CustomError(Exception):
 
 
 def twice_decorator(func):
+    """
+    A decorator that executes the function it wraps twice.
+    :param func: The function that have the property @twice_decorator that been called.
+    :return: Decorator of the inner function.
+    """
     def wrapped_function(function, argument):
+        """
+        Get a function and its argument, call the function with the argument then return its return
+        value.
+        :param function: The function that been called (outside).
+        :param argument: The argument of the function sent.
+        :return:
+        """
         function(argument)
         return function(argument)
     return decorator.decorator(wrapped_function, func)
@@ -14,7 +26,7 @@ def twice_decorator(func):
 
 @twice_decorator
 def times2(num):
-    print("in times2")
+    print("in times2 function")
     return num * 2
 
 
@@ -24,3 +36,4 @@ if __name__ == '__main__':
         times2(5)
     except Exception as e:
         print(e)
+

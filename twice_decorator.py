d06c3f4979153af18b6d5f7f85e362f0a04d3e13
Eliachar Feig <efeig15@gmail.com>
2022-04-30 22:58:33 +0300

    Correction

--- twice_decorator.py
+++ twice_decorator.py
@@ -1,44 +1,30 @@
-from pyclbr import Function
 from typing import Callable
+from decorator import decorator
 
-import decorator
 
-
-def twice_decorator(func: Function) -> Callable:
+@decorator
+def twice_decorator(function: Callable, argument: any) -> Callable:
     """
     A decorator that executes the function it wraps twice.
-
-    :param func: The function that have the property @twice_decorator that been called.
+    :param function: The function that have the property @twice_decorator that been called.
+    :param argument: The argument to the function.
     :return: Decorator of the inner function.
     """
-    def wrapped_function(function, argument) -> int:
-        """
-        Get a function and its argument, call the function with the argument then return its return
-        value.
-
-        :param function: The function that been called (outside).
-        :param argument: The argument of the function sent.
-        :return:
-        """
-        function(argument)
-        return function(argument)
-    return decorator.decorator(wrapped_function, func)
+    function(argument)
+    return function(argument)
 
 
 @twice_decorator
 def times2(num: int) -> int:
     """
     Get an integer and multiply it by 2.
-
     :param num: An integer.
     :return: The integer sent multiply by 2.
     """
-    print("in times2 function")
-    return num * 2
+    print(num)
 
 
 if __name__ == '__main__':
-
     times2(5)
 
 
